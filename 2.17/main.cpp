21a6f1f697c22512b687b7c2e6b8f7d8ccd7d691
xiaozhichuan <1379715558@qq.com>
2019-03-09 23:39:02 +0800

    测绘三班 翟心仪 练习2.17

---
+++ 2.17/main.cpp
@@ -0,0 +1,21 @@
+//Fig2.17£º fig02_17.cpp
+// Printing a line of text with 1~4, and use a space to separate each pair of adjacent numbers.
+#include<iostream> // allows program to output data to the screen
+
+using std::cout; // program uses cout
+using std::cin; // program uses cin
+using std::endl; // program uses endl
+
+//function main begins program execution
+
+int main()
+{
+    cout <<" a) 12 3 4\n" ;
+
+    cout << "b)" << "12 " << "3 " << "4\n" ;
+
+    cout << "c)";
+    cout << "12 ";
+    cout << "3 ";
+    cout << "4";
+} // end function main
